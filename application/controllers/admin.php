07ac1233a4fd99ea09c40dcd05d65cd3f9769f42
Muhammad Andika Pratama <silvhatih@gmail.com>
2021-03-24 09:32:14 +0700

    tambahkan menu tambah data corona (controller)

---
+++ application/controllers/admin.php
@@ -0,0 +1,8 @@
+<?php
+class Admin extends CI_Controller
+{
+    public function input_data_corona()
+    {
+        $this->load->view('admin/input_data');
+    }
+}
---
+++ application/views/admin/v_input_data.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Input Data Corona</title>
+</head>
+
+<body>
+    <form action="method=" post" action="<?php echo base_url('admin/input_topping') ?>"></form>
+</body>
+
+</html>
